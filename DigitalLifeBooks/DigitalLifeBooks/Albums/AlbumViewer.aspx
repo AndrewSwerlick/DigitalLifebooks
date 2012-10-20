f63d1aad55f29d3558ad7b731b9e90b82d803e37
Cameron Smith <logician1989@yahoo.com>
2012-10-20 13:22:32 -0400

    Initial commit of Album viewer page

---
+++ DigitalLifeBooks/DigitalLifeBooks/Albums/AlbumViewer.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AlbumViewer.aspx.cs" Inherits="DigitalLifeBooks.Albums.AlbumViewer"
+%><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+</head>
+<body>
+    <p>Album [<asp:Literal runat="server" ID="AlbumName"></asp:Literal>]
+    for [<asp:Literal runat="server" ID="ChildName"></asp:Literal>]</p>
+</body>
+</html>
---
+++ DigitalLifeBooks/DigitalLifeBooks/Albums/AlbumViewer.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using DigitalLifeBooks.Models;
+
+namespace DigitalLifeBooks.Albums
+{
+    public partial class AlbumViewer : System.Web.UI.Page
+    {
+        string _albumId { get; set; }
+        Album _album { get; set; }
+        Child _child { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            _albumId = Request.QueryString["AlbumId"];
+            if (string.IsNullOrWhiteSpace(_albumId)) return;
+
+            _album = LoadAlbum(_albumId);
+            if (_album == null) return;
+
+            _child = _album.Child;
+
+            AlbumName.Text = _album.Title;
+            ChildName.Text = NameOf(_child);
+        }
+
+        private string NameOf(Child child)
+        {
+            return String.Format("{0} {1}", child.FirstName, child.LastName);
+        }
+
+        private Album LoadAlbum(string albumID)
+        {
+            return null;
+        }
+    }
+}
