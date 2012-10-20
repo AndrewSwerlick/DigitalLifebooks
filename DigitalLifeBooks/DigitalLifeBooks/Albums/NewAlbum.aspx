7a0f67449542600465998c12302566f455d8db9e
Andrew <andrew.swerlick@gmail.com>
2012-10-20 13:54:41 -0400

    adding new page to create new and edit existing album metadata

--- DigitalLifeBooks/DigitalLifeBooks/Albums/NewAlbum.aspx
+++
@@ -1,9 +0,0 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masterpages/Site.Master" AutoEventWireup="true" CodeBehind="NewAlbum.aspx.cs" Inherits="DigitalLifeBooks.Albums.NewAlbum" %>
-
-<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
-</asp:Content>
-
-<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
-    <asp:TextBox ID="Title" runat="server"></asp:TextBox>
-    <asp:Button runat="server" ID="Create" Text="Create" OnClick="On_Create_Clicked" />
-</asp:Content>
--- DigitalLifeBooks/DigitalLifeBooks/Albums/NewOrEditAlbum.aspx.designer.cs
+++ DigitalLifeBooks/DigitalLifeBooks/Albums/NewOrEditAlbum.aspx.designer.cs
@@ -10,16 +10,25 @@
 namespace DigitalLifeBooks.Albums {
     
     
-    public partial class NewAlbum {
+    public partial class NewOrEditAlbum {
         
         /// <summary>
-        /// Title control.
+        /// AlbumTitle control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox Title;
+        protected global::System.Web.UI.WebControls.TextBox AlbumTitle;
+        
+        /// <summary>
+        /// AlbumDescription control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox AlbumDescription;
         
         /// <summary>
         /// Create control.
