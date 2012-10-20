f5a9266f1017f19dd2f8a4c6507a0cb761b69669
Andrew <andrew.swerlick@gmail.com>
2012-10-20 13:14:37 -0400

    Adding new album page

---
+++ DigitalLifeBooks/DigitalLifeBooks/Albums/NewAlbum.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DigitalLifeBooks.Models;
+
+namespace DigitalLifeBooks.Albums
+{
+    public partial class NewAlbum : BaseDLBPage
+    {
+        Child _child { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var childId = Request.QueryString["ChildId"];
+            _child = LoadChild(long.Parse(childId));
+        }
+
+        public void On_Create_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Title.Text))
+                return;
+
+            var album = new Album()
+            {
+                Title = Title.Text,
+                Child = _child
+            };
+
+            DataContext.Albums.AddObject(album);
+            DataContext.SaveChanges();
+            Response.Redirect("Album.aspx?Id" + album.ID);
+        }
+
+        private Child LoadChild(long childId)
+        {
+            return new Child();
+        }
+    }
+}
