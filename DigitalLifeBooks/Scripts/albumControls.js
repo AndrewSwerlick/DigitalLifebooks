0284cccba2cd80832ba9fc8d40fc7465c5fe1844
Anthony Smothers <asmothers@Anthonys-MacBook-Pro.local>
2012-10-20 23:25:09 -0400

    Upgraded album controls. Updated consistent branding css to have new buttons. Added child dashboard with new buttons.

--- DigitalLifeBooks/Scripts/albumControls.js
+++ DigitalLifeBooks/Scripts/albumControls.js
@@ -2,42 +2,56 @@
 
 (function () {
     $(document).ready(function () {
-        $("div.album a.upload").click(function () {
-            var link = $(this);
-            var albumId = link.parent().attr("data-albumId");
+        $("div.album a.upload").on('click', function (e) {
+            var albumId = $(this).parent().data("albumId");
             var iframe = $("<iframe src=/Albums/Upload.aspx?AlbumId=" + albumId + "/>");
             $("body").append(iframe);
-            iframe.dialog();
+            iframe.dialog({
+                draggable: false,
+                height: 400,
+                width: 996,
+                modal: true,
+                resizable: false,
+                title: 'foo'
+            });
 
             window.closeIframe = function () {
                 iframe.dialog('close');
                 document.location.reload(true);
                 return false;
             }
+            e.preventDefault();
         });
 
-        $("a.newAlbum").click(function () {
+        $("a.newAlbum").on('click', function (e) {
             var link = $(this);
-            var childId = link.attr("data-childId");
+            var childId = $(this).data("childId");
             var iframe = $("<iframe src=/Albums/NewOrEditAlbum.aspx?ChildId=" + childId + "/>");
             $("body").append(iframe);
-            iframe.dialog();
-
+            iframe.dialog({
+                draggable: false,
+                height: 400,
+                width: 996,
+                modal: true,
+                resizable: false,
+                title: 'foo'
+            });
             window.closeIframe = function () {
                 iframe.dialog('close');
                 document.location.reload(true);
                 return false;
             }
+            e.preventDefault();
         });
 
-        $("a.delete").click(function () {
-            var link = $(this);
-            var id = link.attr("data-entityId");
-            var type = link.attr("data-entityType");
+        $("a.delete").on('click', function (e) {
+            var id = $(this).data("entityId");
+            var type = $(this).data("entityType");
 
             $.post('/Services/Delete.asmx/DeleteEntity', "EntityType=" + type + "&EnityID=" + id, function (data) {
                 document.location.reload(true);
             });
+            e.preventDefault();
         });
 
     });
