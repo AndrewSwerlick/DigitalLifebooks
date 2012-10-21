516f7eddd480179d54a17fda35b1842ccc2c435d
Andrew <andrew.swerlick@gmail.com>
2012-10-20 20:02:52 -0400

    wiring up uplaod functionality in jquery modal

--- DigitalLifeBooks/DigitalLifeBooks/ChildProfile/Dashboard.aspx
+++ DigitalLifeBooks/DigitalLifeBooks/ChildProfile/Dashboard.aspx
@@ -4,7 +4,7 @@
 
 
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
-    <script type="text/javascript" src="../Scripts/albumControls.js"></script>
+    <script type="text/javascript" src="/Scripts/albumControls.js"></script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
     <img src='<%= ProfilePicLink %>' alt=<%= Child.FirstName + Child.LastName %> />
--- DigitalLifeBooks/DigitalLifeBooks/Scripts/albumControls.js
+++ DigitalLifeBooks/DigitalLifeBooks/Scripts/albumControls.js
@@ -1,7 +1,22 @@
-﻿(function () {
+﻿
+
+(function () {
     $(document).ready(function () {
-        
-    
+        $("div.album a.upload").click(function () {
+            var link = $(this);
+            var albumId = link.parent().attr("data-albumId");
+            var iframe = $("<iframe src=/Albums/Upload.aspx?AlbumId=" + albumId + "/>");
+            $("body").append(iframe);
+            iframe.dialog();
+
+            window.closeIframe = function () 
+            {
+                iframe.dialog('close');
+                document.location.reload(true);
+                return false;
+            }
+        });
+
     });
 
 
